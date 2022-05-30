c948c493a68eb8f979f95c5626138f3c045be2e9
liu-chunzhang <lkz9808@163.com>
2022-05-30 20:59:00 +0800

    exercise 09

---
+++ exe09/task2/BDF/benzene/ben-cas66-ccpvtz.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J intel
+#BSUB -o ben-cas66-ccpvtz.out
+#BSUB -n 8
+#BSUB -q cqueue
+#BSUB -R "span[hosts=1]"
+
+export BDFHOME=/apps/users/lcz/bdf-pkg
+export BDF_TMPDIR=/scratch/ben-cas66-ccpvtz_283
+export USE_LIBCINT=no
+export LD_LIBRARY_PATH=/apps/users/lcz/bdf-pkg/extlibs:/apps/users/lcz/bdf-pkg/libso:/apps/software/intel2019/mkl/lib/intel64:/apps/software/intel2019/compilers_and_libraries_2019/linux/lib/intel64:$LD_LIBRARY_PATH
+ulimit -s unlimited
+ulimit -t unlimited
+export OMP_NUM_THREADS=8
+export OMP_STACKSIZE=20G
+
+/apps/users/lcz/bdf-pkg/sbin/bdfdrv.py -tmpdir /scratch/ben-cas66-ccpvtz_283 -r ben-cas66-ccpvtz.inp > ben-cas66-ccpvtz.out
+rm -rf .ben-cas66-ccpvtz.wrk
---
+++ exe09/task5/BDF/Mul5/FeH2O62+-mul5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J intel
+#BSUB -o FeH2O62+-mul5.out
+#BSUB -n 8
+#BSUB -q cqueue
+#BSUB -R "span[hosts=1]"
+
+export BDFHOME=/apps/users/lcz/bdf-pkg
+export BDF_TMPDIR=/scratch/FeH2O62+-mul5_628
+export USE_LIBCINT=no
+export LD_LIBRARY_PATH=/apps/users/lcz/bdf-pkg/extlibs:/apps/users/lcz/bdf-pkg/libso:/apps/software/intel2019/mkl/lib/intel64:/apps/software/intel2019/compilers_and_libraries_2019/linux/lib/intel64:$LD_LIBRARY_PATH
+ulimit -s unlimited
+ulimit -t unlimited
+export OMP_NUM_THREADS=8
+export OMP_STACKSIZE=20G
+
+/apps/users/lcz/bdf-pkg/sbin/bdfdrv.py -tmpdir /scratch/FeH2O62+-mul5_628 -r FeH2O62+-mul5.inp > FeH2O62+-mul5.out
+rm -rf .FeH2O62+-mul5.wrk
