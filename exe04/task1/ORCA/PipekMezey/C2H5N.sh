2927dfaebb1ad889206e66d1fff42478a47a6dd6
liu-chunzhang <lkz9808@163.com>
2022-06-24 07:44:47 +0800

    exercise 04

--- exe04/task1/ORCA/PipekMezey/C2H5N.sh
+++ exe04/task1/ORCA/PipekMezey/C2H5N.sh
@@ -3,9 +3,13 @@
 #BSUB -o C2H5N.out
 #BSUB -n 8
 #BSUB -q cqueue
-#BSUB -R "span[hosts=1]"
 
 export LD_LIBRARY_PATH=/apps/software/openmpi-4.1.1/lib:/apps/software/orca_5_0_0:$LD_LIBRARY_PATH
 export PATH=/apps/software/openmpi-4.1.1/bin:/apps/software/orca_5_0_0:$PATH
 
-/apps/software/orca_5_0_0/orca C2H5N.inp >& C2H5N.out
+mkdir /scratch/C2H5N_755
+scp $USER@mgt01:/apps/users/lcz/loc/C2H5N.inp /scratch/C2H5N_755
+/apps/software/orca_5_0_0/orca /scratch/C2H5N_755/C2H5N.inp >& C2H5N.out
+scp /scratch/C2H5N_755/*.gbw $USER@mgt01:/apps/users/lcz/loc
+scp /scratch/C2H5N_755/*.xyz $USER@mgt01:/apps/users/lcz/loc
+scp /scratch/C2H5N_755/*.trj $USER@mgt01:/apps/users/lcz/loc
