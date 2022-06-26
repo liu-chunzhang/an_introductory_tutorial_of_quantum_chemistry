f4f8b49b8b925277ee8d058a11f90b3337ba3ce2
liu-chunzhang <lkz9808@163.com>
2022-06-26 18:18:00 +0800

    exercise 11

---
+++ exe11/BDF-FrozenCore-VVCI/cytosine-MRCI-14-10.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J intel
+#BSUB -o cytosine-MRCI-14-10.out
+#BSUB -n 18
+#BSUB -q cqueue
+#BSUB -R "span[hosts=1]"
+
+export BDFHOME=/apps/software/bdf-pkg/build/bdf-pkg-full
+export BDF_TMPDIR=/scratch/cytosine-MRCI-14-10_964
+export USE_LIBCINT=no
+export LD_LIBRARY_PATH=/apps/software/bdf-pkg/build/bdf-pkg-full/extlibs:/apps/software/bdf-pkg/build/bdf-pkg-full/libso:/apps/software/intel2019/mkl/lib/intel64:/apps/software/intel2019/compilers_and_libraries_2019/linux/lib/intel64:$LD_LIBRARY_PATH
+ulimit -s unlimited
+ulimit -t unlimited
+export OMP_NUM_THREADS=18
+export OMP_STACKSIZE=30G
+
+/apps/software/bdf-pkg/build/bdf-pkg-full/sbin/bdfdrv.py -tmpdir /scratch/cytosine-MRCI-14-10_964 -r cytosine-MRCI-14-10.inp > cytosine-MRCI-14-10.out
+rm -rf .cytosine-MRCI-14-10.wrk
---
+++ exe11/BDF-FrozenCore-VVCI/cytosine-RHF.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J intel
+#BSUB -o cytosine-RHF.out
+#BSUB -n 8
+#BSUB -q cqueue
+#BSUB -R "span[hosts=1]"
+
+export BDFHOME=/apps/software/bdf-pkg/build/bdf-pkg-full
+export BDF_TMPDIR=/scratch/cytosine-RHF_611
+export USE_LIBCINT=no
+export LD_LIBRARY_PATH=/apps/software/bdf-pkg/build/bdf-pkg-full/extlibs:/apps/software/bdf-pkg/build/bdf-pkg-full/libso:/apps/software/intel2019/mkl/lib/intel64:/apps/software/intel2019/compilers_and_libraries_2019/linux/lib/intel64:$LD_LIBRARY_PATH
+ulimit -s unlimited
+ulimit -t unlimited
+export OMP_NUM_THREADS=8
+export OMP_STACKSIZE=20G
+
+/apps/software/bdf-pkg/build/bdf-pkg-full/sbin/bdfdrv.py -tmpdir /scratch/cytosine-RHF_611 -r cytosine-RHF.inp > cytosine-RHF.out
+rm -rf .cytosine-RHF.wrk
